4130830c764b7f46ec7b2bb1e3d7a59dfb4803c6
Yusuke Kamiyamane <y@y14e.com>
2025-02-17 23:23:04 +0900

    Update text-splitter.ts

--- text-splitter.ts
+++ text-splitter.ts
@@ -102,7 +102,7 @@ class TextSplitter {
     const list = this[`${by}s` as 'words' | 'chars'];
     [...node.childNodes].forEach(node => {
       if (node.nodeType === 3) {
-        const segments = [...new Intl.Segmenter(((node.parentNode as HTMLElement).closest('[lang]') as HTMLElement)?.getAttribute('lang') || document.documentElement.getAttribute('lang') || 'en', by === 'word' && this.props.wordSegmenter ? { granularity: 'word' } : {}).segment(node.textContent!.replace(/[\r\n\t]/g, '').replace(/\s{2,}/g, ' '))];
+        const segments = [...new Intl.Segmenter(((node.parentNode as HTMLElement).closest('[lang]') as HTMLElement).getAttribute('lang') || document.documentElement.getAttribute('lang') || 'en', by === 'word' && this.props.wordSegmenter ? { granularity: 'word' } : {}).segment(node.textContent!.replace(/[\r\n\t]/g, '').replace(/\s{2,}/g, ' '))];
         segments.forEach(segment => {
           const element = document.createElement('span');
           const text = segment.segment || ' ';
@@ -127,7 +127,7 @@ class TextSplitter {
     let previous = null;
     for (let i = 0; i < list.length; i++) {
       const item = list[i];
-      if (previous && previous.textContent!.trim() && LBR_PROHIBIT_START_REGEXP.test([...new Intl.Segmenter((item.closest('[lang]') as HTMLElement)?.getAttribute('lang') || document.documentElement.getAttribute('lang') || 'en').segment(item.textContent!)].shift()!.segment)) {
+      if (previous && previous.textContent!.trim() && LBR_PROHIBIT_START_REGEXP.test([...new Intl.Segmenter((item.closest('[lang]') as HTMLElement).getAttribute('lang') || document.documentElement.getAttribute('lang') || 'en').segment(item.textContent!)].shift()!.segment)) {
         previous.setAttribute(`data-${by}`, (previous.textContent += item.textContent!));
         item.remove();
         list.splice(i, 1);
