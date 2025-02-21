5f29f4b2341c8da8957a22987877cf4a088e4cb5
Yusuke Kamiyamane <y@y14e.com>
2025-02-21 20:49:14 +0900

    20250221

--- text-splitter.ts
+++ text-splitter.ts
@@ -74,6 +74,7 @@ class TextSplitter {
     [...this.element.querySelectorAll(':scope > :not([data-word]) [data-char][data-whitespace]')].forEach(whitespace => {
       if (window.getComputedStyle(whitespace).getPropertyValue('display') !== 'inline') whitespace.innerHTML = '&nbsp;';
     });
+    this.element.setAttribute('data-text-splitter-initialized', '');
   }
 
   private nobr(node = this.dom): void {
